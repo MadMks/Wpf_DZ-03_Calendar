6b85d4a0f5f857eb140ff910ea04af38e322a9a2
T500 <mks59k@gmail.com>
2018-06-19 13:01:06 +0300

    Working.

--- Task_Calendar/MainWindow.xaml.cs
+++ Task_Calendar/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace Task_Calendar
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int currentMonth = DateTime.Today.Month;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void treeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+            switch ((treeView.SelectedItem as TreeViewItem).Header.ToString())
+            {
+                case "Декабрь":
+                    currentMonth = 12;
+                    break;
+                case "Январь":
+                    currentMonth = 1;
+                    break;
+                case "Февраль":
+                    currentMonth = 2;
+                    break;
+                case "Март":
+                    currentMonth = 3;
+                    break;
+                case "Апрель":
+                    currentMonth = 4;
+                    break;
+                case "Май":
+                    currentMonth = 5;
+                    break;
+                case "Июнь":
+                    currentMonth = 6;
+                    break;
+                case "Июль":
+                    currentMonth = 7;
+                    break;
+                case "Август":
+                    currentMonth = 8;
+                    break;
+                case "Сентябрь":
+                    currentMonth = 9;
+                    break;
+                case "Октябрь":
+                    currentMonth = 10;
+                    break;
+                case "Ноябрь":
+                    currentMonth = 11;
+                    break;
+                default:
+                    break;
+            }
+
+            this.calendarListView.DisplayDate = new DateTime(DateTime.Today.Year, currentMonth, DateTime.Today.Day);
         }
     }
 }
